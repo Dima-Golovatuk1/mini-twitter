861af58346c493e08397b592e16806c0cb67f646
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2024-10-08 18:52:53 +0300

    index

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.secret_key = '-^c^e%1q4n%rc^fr6k5u$6#&_4e801ctf3%sro=_xycfcu5%qul'
 
-@app.route()
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
