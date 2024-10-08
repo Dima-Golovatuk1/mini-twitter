c770780f780f140ce23c3530e86a115747941688
LytvyxEz <maxlytw@gmail.com>
2024-10-08 18:32:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, redirect
 
 
 app = Flask(__name__)
 app.secret_key = '-^c^e%1q4n%rc^fr6k5u$6#&_4e801ctf3%sro=_xycfcu5%qul'
 
-@app.route()
+
+@app.route('/')
+def Home():
+    render_template('index.html')
+
+
+@app.route('/')
+def Explore():
+    render_template('index.html')
+
+
+@app.route('/')
+def Messages():
+    render_template('index.html')
+
+
+@app.route('/')
+def Bookmarks():
+    render_template('index.html')
+
+
+@app.route('/')
+def Profile():
+    render_template('index.html')
