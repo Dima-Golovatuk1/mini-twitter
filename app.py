65260451b2da41b8b290b906992b64e6281008c6
Dmitro Oleynik <dmjtrooleinik@gmail.com>
2024-10-08 19:17:33 +0300

    Harons update

--- app.py
+++ app.py
@@ -8,27 +8,28 @@ app.secret_key = '-^c^e%1q4n%rc^fr6k5u$6#&_4e801ctf3%sro=_xycfcu5%qul'
 
 @app.route('/')
 def home():
-    render_template('index.html')
+    return render_template('index.html')
 
 
-@app.route('/')
+@app.route('/explore')
 def explore():
-    render_template('index.html')
+    return render_template('index.html')
 
 
-@app.route('/')
+@app.route('/messages')
 def messages():
-    render_template('index.html')
+    return render_template('index.html')
 
 
-@app.route('/')
+@app.route('/bookmarks')
 def bookmarks():
-    render_template('index.html')
+    return render_template('index.html')
 
 
-@app.route('/')
+@app.route('/profile')
 def profile():
-    render_template('index.html')
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
