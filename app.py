7ae6ae45f3fe7decfa36d4a8a2e9bb6d8c541b8b
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2024-10-09 17:08:33 +0300

    register

--- app.py
+++ app.py
@@ -11,6 +11,16 @@ def Home():
     return render_template('index.html')
 
 
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    return render_template('register.html')
+
+
 @app.route('/Explore')
 def Explore():
     return render_template('explore.html')
