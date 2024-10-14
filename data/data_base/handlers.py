c29a4021faba6c968d83d07cc1ebc2a530c006a8
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2024-10-14 15:14:57 +0300

    updata

--- data/data_base/handlers.py
+++ data/data_base/handlers.py
@@ -1,101 +1,20 @@
-import psycopg2
-import logging
+from supabase import create_client, Client
+
+url = "https://igckzyzeayjxipwqpzuh.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnY2t6eXplYXlqeGlwd3FwenVoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODc1MTY3MSwiZXhwIjoyMDQ0MzI3NjcxfQ.wmXPXkmeUUNJNhrPcFdz8qCUn1wc6bUf-LFRLrGS4HU"
+
+supabase: Client = create_client(url, key)
 
 
-from config import *
 
-
-def get_db_connection():
-    connection = psycopg2.connect(
-        host=host,
-        database=db_name,
-        user=user,
-        password=password
-    )
-    return connection
-
-
-def get_post_by_id(post_id):
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    query = "SELECT post_name, content FROM posts WHERE id = %s"
-    cursor.execute(query, (post_id,))
-    post = cursor.fetchone()
-    cursor.close()
-    connection.close()
-
-    if post:
-        post_name, content = post
-        return {
-            'post_name': post_name,
-            'content': content
-        }
-    else:
-        return None
-
-
-def add_new_user(name: str, email: str, hash_password: str, birthday: str, sex: str):
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        query = """INSERT INTO users (id, name, email, password, birthday, sex) VALUES (%s, %s, %s, %s, %s, %s)"""
-        cursor.execute(
-            query, (1, name, email, hash_password, birthday, sex)
-        )
-        connection.commit()
-        return "success"
-
-    except Exception as ex:
-        logging.error('[INFO] Error while working with PostgreSQL', exc_info=True)
-
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            logging.info('[INFO] PostgreSQL connection closed')
-
-
-def look_user_by_id(_id: int):
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        query = """SELECT name, email, password, birthday, sex FROM users WHERE id = %s"""
-        cursor.execute(query, (_id,))
-        user = cursor.fetchone()
-        if user:
-            return {
-                "name": user[0],
-                "email": user[1],
-                "password": user[2],
-                "birthday": user[3],
-                "sex": user[4]
-            }
-        else:
-            return "User not found"
-
-    except Exception as ex:
-        logging.error('[INFO] Error while working with PostgreSQL', exc_info=True)
-
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            logging.info('[INFO] PostgreSQL connection closed')
-
-
-def delete_user_by_id(_id: int):
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        query = """DELETE FROM users WHERE id = %s"""
-        cursor.execute(query, (_id,))
-        connection.commit()
-
-    except Exception as ex:
-        logging.error('[INFO] Error while working with PostgreSQL', exc_info=True)
-
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            logging.info('[INFO] PostgreSQL connection closed')
+# response = (
+#     supabase.table("users")
+#     .insert({"name": "Denmarkhf",
+#              'email': 'adafhsd@gmai;.com',
+#              'password': 146831,
+#              'birthday': '06-12-2000',
+#              'sex': 'man'})
+#     .execute()
+# )
+response = supabase.table("users").select("*").execute()
+print(response.data[0])
