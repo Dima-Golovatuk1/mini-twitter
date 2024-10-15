d778e88be9c6e06db8a58bc78444397943aa348c
Dmitro Oleynik <dmjtrooleinik@gmail.com>
2024-10-15 17:17:15 +0300

    1

---
+++ data/data_base/handlers.py
@@ -0,0 +1,20 @@
+from supabase import create_client, Client
+from config import *
+
+
+supabase: Client = create_client(url, key)
+
+
+def add_user_to_users(name, email, password, birthday, sex):
+    data = {
+        'name': name,
+        'email': email,
+        'password': password,
+        'birthday': birthday,
+        'sex': sex
+    }
+    response = supabase.table('users').insert(data).execute()
+
+
+def delete_user_from_users(name):
+    response = supabase.table('users').delete().eq('name', name).execute()
