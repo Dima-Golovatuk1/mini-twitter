3d910b7c8d60c7be1261f5cda18ac7bfca6f14ed
Dmitro Oleynik <dmjtrooleinik@gmail.com>
2024-10-15 17:53:21 +0300

    1

--- data/data_base/handlers.py
+++ data/data_base/handlers.py
@@ -16,10 +16,17 @@ def add_user_to_users(name, email, password, birthday, sex):
     response = supabase.table('users').insert(data).execute()
 
 
-def get_users_by_id(user_id):
+def get_user_by_id(user_id):
     response = supabase.table("users").select("*").eq("id", user_id).execute()
+    return response
+
 
 def delete_user_from_users(name):
     response = supabase.table('users').delete().eq('name', name).execute()
 
 
+def plus_one_new_follower(user_id):
+    response = supabase.table('users')\
+        .update({'followers': supabase.table('users').select('followers')\
+                .eq('id', user_id).execute().data[0]['followers'] + 1})\
+        .eq('id', user_id).execute()
