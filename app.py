d279e9465f823aed2d27fbe45eae74f26ddd8d52
Dmitro Oleynik <dmjtrooleinik@gmail.com>
2024-10-20 12:25:40 +0300

    1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from werkzeug.security import generate_password_hash, check_password_hash
 from data.data_base.handlers import (
     get_users_by_id, add_user_to_users, get_users, get_users_by_email,
-    get_all_posts, get_all_posts_by_user_id, create_new_post, checking_email, checking_name)
+    get_all_posts, get_all_posts_by_user_id, create_new_post)
 from data.data_base.handlers import (get_users_by_id, add_user_to_users, get_users, get_users_by_email, get_all_posts,
                                      get_all_posts_by_user_id)
 from email_validator import validate_email, EmailNotValidError
@@ -280,7 +280,7 @@ def addpost():
 def post(post_id):
     post_data = get_post_by_id(post_id)
     if post_data:
-        post_name = post_data['post_name']
+        post_name = post_data['title']
         content = post_data['content']
         return render_template('post.html', post_name=post_name, content=content, post_id=post_id)
     else:
