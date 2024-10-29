706c271f362ca0ea2ec7156b0bcb3765365a65ef
Dmitro Oleynik <dmjtrooleinik@gmail.com>
2024-10-29 16:48:49 +0200

    update

--- app.py
+++ app.py
@@ -252,6 +252,7 @@ def view_profile(id):
         flash("That user doesn't exist", 'danger')
         return redirect(url_for('home'))
 
+
 @app.route('/global')
 @login_required
 def global_page():
@@ -268,7 +269,7 @@ def following():
     name = current_user.name
     user_id = current_user.id
     posts = []
-    all_post = get_all_posts()
+    all_post = get_all_post_by_follower(user_id)
     return render_template('following.html', user_id=user_id, username=name, posts=posts, all_post=all_post)
 
 
