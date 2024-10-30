77848b199684ec2c251e3aea943fcab9535a68a2
Dmitro Oleynik <dmjtrooleinik@gmail.com>
2024-10-30 19:16:17 +0200

    update

--- data/data_base/handlers.py
+++ data/data_base/handlers.py
@@ -153,8 +153,8 @@ def get_user_id_by_post_id(post_id):
 
 def get_all_post_by_follower(follower_id: int):
     try:
-        response = supabase.table('followers').select('*').eq('follower_id', follower_id).execute()
-        list_followers = [item['user_id'] for item in response.data[:]]
+        response = supabase.table('followers').select('*').eq('user_id', follower_id).execute()
+        list_followers = [item['follower_id'] for item in response.data[:]]
         if not list_followers:
             return []
         posts_response = supabase.table('posts').select('*').in_('user_id', list_followers).execute()
